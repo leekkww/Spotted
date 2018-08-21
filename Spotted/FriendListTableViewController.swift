450dd25c02dc8e2c730cf5f0cd38f0aa10c700a1
leekkww <leekkww@gmail.com>
2018-08-21 10:29:12 -0700

    friend list beginnings

--- Spotted/FriendListTableViewController.swift
+++ Spotted/FriendListTableViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class FriendListTableViewController : UITableView {
-
+class FriendListTableViewController : UITableViewController {
+    var headlines = [
+        Headline(id: 1, title: "Lorem Ipsum", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", image: "Apple"),
+        Headline(id: 2, title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non.", image: "Banana"),
+        Headline(id: 3, title: "In ac ante sapien", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc.", image: "Cantaloupe"),
+        ]
 
 }
