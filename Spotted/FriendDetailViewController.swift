45bfe730700c27b038776e764d355412ac269c8a
leekkww <leekkww@gmail.com>
2018-08-22 01:15:39 -0700

    search bar for friendos

---
+++ Spotted/FriendDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FriendDetailViewController.swift
+//  Spotted
+//
+//  Created by Joanne Lee on 8/21/18.
+//  Copyright © 2018 Spotted. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var candyImageView: UIImageView!
+    
+    var detailCandy: Friend? {
+        didSet {
+            configureView()
+        }
+    }
+    
+    func configureView() {
+        if let detailCandy = detailCandy {
+            if let detailDescriptionLabel = detailDescriptionLabel, let candyImageView = candyImageView {
+                detailDescriptionLabel.text = detailCandy.name
+                candyImageView.image = UIImage(named: detailCandy.name)
+                //title = detailCandy.category
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
--- Spotted/FriendListTableViewController.swift
+++ Spotted/FriendListTableViewController.swift
@@ -15,31 +15,111 @@ struct Friend {
     
 }
 
-class FriendListTableViewController : UITableViewController {
-    
+class FriendListTableViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+
+    @IBOutlet var tableView: UITableView!
+
     var friendos = [
         Friend(id: 1, name: "Joan"),
         Friend(id: 2, name: "Mike"),
         Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        Friend(id: 1, name: "Joan"),
+        Friend(id: 2, name: "Mike"),
+        Friend(id: 3, name: "Slov"),
+        
     ]
+
+    var filteredFriends = [Friend]()
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredFriends = friendos.filter({( friend : Friend) -> Bool in
+            return friend.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.placeholder = "Search Friendos"
+        tableView.tableHeaderView = searchController.searchBar;
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering() {
+            return filteredFriends.count
+        }
         return friendos.count
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Your friendos"
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Recent friendos"
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath)
         
-        cell.textLabel?.text = friendos[indexPath.row].name
+        let friendo: Friend
+        if isFiltering() {
+            friendo = filteredFriends[indexPath.row]
+        } else {
+            friendo = friendos[indexPath.row]
+        }
+        
+        cell.textLabel!.text = friendo.name
+        //cell.detailTextLabel!.text = candy.category
         
         return cell
     }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "showDetail" {
+//            if let indexPath = tableView.indexPathForSelectedRow {
+//                let friend = friendos[indexPath.row]
+//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//                controller.detailCandy = friend
+//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+//            }
+//        }
+//    }
 }
